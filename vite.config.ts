48c00b4cd87a5fa5595ba2979b7a47b12ac5d822
Erwan BERNARD <bernard.erwan@gmail.com>
2025-09-13 10:08:21 +0200

    feat: rename project

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://edmbernard.github.io/time-since-web/",
+  base: "https://edmbernard.github.io/noteit/",
 })
