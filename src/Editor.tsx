f7013c3354aa73a5f3ea69cb08e00fc5df4034d8
Erwan BERNARD <bernard.erwan@gmail.com>
2025-09-13 09:57:12 +0200

    feat: add simple styling

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -4,11 +4,13 @@ import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
+import type { EditorThemeClasses } from 'lexical';
+import './Editor.css';
 
-const theme = {
-  // Theme styling goes here
-  //...
-}
+const theme: EditorThemeClasses = {
+  // Root element of the content editable area
+  root: 'editor-root'
+};
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -26,15 +28,17 @@ function Editor() {
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={
-          <ContentEditable
-            aria-placeholder={'Enter some text...'}
-            placeholder={<div>Enter some text...</div>}
-          />
-        }
-        ErrorBoundary={LexicalErrorBoundary}
-      />
+      <div className="editor-wrapper">
+        <RichTextPlugin
+          contentEditable={
+            <ContentEditable
+              aria-placeholder="Enter some text..."
+              placeholder={<div className="editor-placeholder">Enter some text...</div>}
+            />
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
       <HistoryPlugin />
       <AutoFocusPlugin />
     </LexicalComposer>
